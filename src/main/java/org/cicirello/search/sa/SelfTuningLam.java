53e5fdabff848b7bf552c00baf3c8ca25bb50d61
Vincent A. Cicirello <cicirello@users.noreply.github.com>
2021-08-26 11:25:10 -0400

    Update SelfTuningLam.java

--- src/main/java/org/cicirello/search/sa/SelfTuningLam.java
+++ src/main/java/org/cicirello/search/sa/SelfTuningLam.java
@@ -239,6 +239,10 @@ public final class SelfTuningLam implements AnnealingSchedule {
 			t = costAverage * (lastMaxEvals >= 10000 ? 0.3141120890121576 : 0.18987910472222955);
 			// The above is logically equivalent to:
 			// t = -costAverage / Math.log(0.001 / (1.001 - acceptRate));
+			
+			// The following is equivalent to computing:
+			// The M-th root of Zeta_{2M} / Zeta_{M}, where M is phase0,
+			// Zeta_M is -1 / Math.log(0.001 / (1.001 - LamRate(M))).
 			beta = Math.pow(
 				lastMaxEvals >= 10000 ? 0.8300587656396743: 0.912935823058667,
 				1.0 / phase0
